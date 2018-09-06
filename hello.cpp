f7725269bac255ee76082f63928c877964c7134c
gavind20 <gjdacumos@mail.csuchico.edu>
2018-09-05 17:20:35 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
+//Gavin Dacumos
+
 #include <iostream>
 #include <string>
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Gavin. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
